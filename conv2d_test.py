fd5d2081d63ce725cb366e5ce36cd4d6f766385d
Sam <sameerrajmalik@gmail.com>
2024-01-31 10:58:54 -0800

    Implemented numpy backend for speedups

--- conv2d_test.py
+++ conv2d_test.py
@@ -1,8 +1,9 @@
 import match
 
-x = match.randn(4, 3, 32, 16, generator=lambda: 1)
+use_numpy = True
+x = match.randn(16, 3, 32, 32, generator=lambda: 1, use_numpy=use_numpy)
 
-conv2d_layer = match.nn.Conv2d(in_channels=3, out_channels=20, kernel_size=(2,3))
+conv2d_layer = match.nn.Conv2d(in_channels=3, out_channels=20, kernel_size=3, use_numpy=use_numpy)
 
 if __name__ == "__main__":
        conv2d_layer(x)
--- match/__init__.py
+++ match/__init__.py
@@ -4,17 +4,28 @@ from .nn import *
 from operator import add, ge, gt, le, lt, mul, pow
 from math import exp, ceil, prod
 from random import gauss
+import numpy as np
 
 
-def randn(*shape, generator=lambda: gauss(0, 1)) -> Tensor:
-    if isinstance(shape[0], tuple):
-        shape = shape(0)
-    if not shape:
-        return Tensor(TensorData(value=generator()))
+def randn(*shape, generator=lambda: gauss(0, 1), use_numpy=False) -> Tensor:
+        if isinstance(shape[0], tuple):
+            shape = shape[0]
 
-    rand_tensordata = TensorData(0)
-    data = [TensorData(value=generator()) for _ in range(prod(shape))]
-    rand_tensordata._data = data
-    rand_tensordata.reshape_(shape)
-    return Tensor(rand_tensordata)
+        if use_numpy:
+            rng = np.random.default_rng(seed=47)
+            data = TensorData(
+                *shape,
+                use_numpy=True,
+                numpy_data=rng.random(shape),
+            )
+            return Tensor(data=data)
+
+        if not shape:
+            return Tensor(TensorData(value=generator()))
+
+        rand_tensordata = TensorData(0)
+        data = [TensorData(value=generator()) for _ in range(prod(shape))]
+        rand_tensordata._data = data
+        rand_tensordata.reshape_(shape)
+        return Tensor(rand_tensordata)
 
