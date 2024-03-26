640953e063d1d545c1688561c99b80450a61aa56
anthonyjclark <anthonyjclark@gmail.com>
2024-03-26 14:40:52 -0700

    Renamed tensordata to tensorbase.

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 from setuptools import Extension, setup
 
-tensordata_module = Extension("match.tensordata", sources=["src/tensordata.c"])
+tensordata_module = Extension("match.tensorbase", sources=["src/tensorbase.c"])
 
 setup(ext_modules=[tensordata_module])
