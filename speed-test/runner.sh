772f352fccf65ee534f6003d3bbfe214aeb61253
anthonyjclark <anthonyjclark@gmail.com>
2024-04-26 14:10:05 -0700

    Add c-extension to runner.

--- speed-test/runner.sh
+++ speed-test/runner.sh
@@ -13,8 +13,8 @@ python harness.py torch "" --mnist-size=1000
 python harness.py torch "" --mnist-size=60000
 
 MATRIX_BACKEND=loops NUMBA_DISABLE_JIT=1 python harness.py matrix "loops.nojit" --mnist-size=10
-MATRIX_BACKEND=loops NUMBA_DISABLE_JIT=1 python harness.py matrix "loops.nojit" --mnist-size=1000
 MATRIX_BACKEND=loops NUMBA_DISABLE_JIT=1 python harness.py matrix "loops.nojit" --mnist-size=100
+MATRIX_BACKEND=loops NUMBA_DISABLE_JIT=1 python harness.py matrix "loops.nojit" --mnist-size=1000
 MATRIX_BACKEND=loops NUMBA_DISABLE_JIT=1 python harness.py matrix "loops.nojit" --mnist-size=60000
 
 MATRIX_BACKEND=loops NUMBA_DISABLE_JIT=0 python harness.py matrix "loops.jit" --mnist-size=10
@@ -31,3 +31,8 @@ MATRIX_BACKEND=listcomp NUMBA_DISABLE_JIT=0 python harness.py matrix "listcomp-j
 MATRIX_BACKEND=listcomp NUMBA_DISABLE_JIT=0 python harness.py matrix "listcomp-jit" --mnist-size=100
 MATRIX_BACKEND=listcomp NUMBA_DISABLE_JIT=0 python harness.py matrix "listcomp-jit" --mnist-size=1000
 MATRIX_BACKEND=listcomp NUMBA_DISABLE_JIT=0 python harness.py matrix "listcomp-jit" --mnist-size=60000
+
+python harness.py extension "" --mnist-size=10
+python harness.py extension "" --mnist-size=100
+python harness.py extension "" --mnist-size=1000
+python harness.py extension "" --mnist-size=60000
